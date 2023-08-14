49656e6f7825fdb94fc0862864f4f519e75f3976
D0D0C <138598755+D0D0C@users.noreply.github.com>
2023-08-14 12:05:03 +0200

    Update Hotel.java - methode ajouter client

--- demo_POO/src/main/java/org/example/hotel/Hotel.java
+++ demo_POO/src/main/java/org/example/hotel/Hotel.java
@@ -1,15 +1,28 @@
-package org.example.hotel;
+package Hotel;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Hotel {
 
     private ArrayList<Chambre> chambres;
 
-    private ArrayList<Client>client;
+    private ArrayList<Client> client;
 
-    private ArrayList<Reservation>reservation;
+    private ArrayList<Reservation> reservation;
 
+      public class ajouterClient {
+        private List<Client> clients;
+        private List<Chambre> chambres;
+        private List<Reservation> reservations;
+
+        public void Hotel() {
+            clients = new ArrayList<>();
+            chambres = new ArrayList<>();
+            reservations = new ArrayList<>();
+        }
+
+    }
 }
 
 
